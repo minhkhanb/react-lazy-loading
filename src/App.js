a82b88267332e9766597ef457db72c4d061ef425
Kha Le <minhkhanb@gmail.com>
2019-10-22 17:17:02 -0700

    check spotify link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import GridImages from './GridImages';
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <GridImages></GridImages>
       </header>
     </div>
--- src/GridImages.js
+++ src/GridImages.js
@@ -1,11 +1,75 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import axios from 'axios'
 
 class GridImages extends Component {
-  render() {
-    return(
-      <div>GridImages</div>
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      images: []
+    }
+  }
+
+  async getImagesFromApi () {
+    const result = await axios({
+      method: 'post',
+      url: 'https://listings.salespaddock.com/api/GetAroundYouUsers',
+      headers: {
+        'Content-Type': 'application/json',
+        'horse': 123456
+      },
+      data: {'userId': '0', 'long': -70.648258253932, 'lat': 43.1617483003778, 'redious': '2000'}
+    })
+    const {response: imgList} = result.data
+    console.log('imgList: ', imgList)
+    this.setState({
+      images: imgList
+    })
+  }
+
+  componentDidMount () {
+    this.getImagesFromApi()
+  }
+
+  renderItems () {
+    const {images} = this.state
+    const listItems = images.map((img) => {
+      if (!!img.image) {
+        return (
+          <li key={img.userId}>
+            <div className="img">
+              <img src={img.image} alt=""/>
+            </div>
+          </li>
+        )
+      }
+      return (
+        <li key={img.userId}>
+          <div className="img">
+            <div className="defaultImg"></div>
+          </div>
+        </li>
+      )
+    })
+    return (
+      <div className="grid">
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>GridImages</h1>
+        <div>
+          {this.renderItems()}
+        </div>
+      </div>
     )
   }
 }
 
-export default GridImages;
+export default GridImages
